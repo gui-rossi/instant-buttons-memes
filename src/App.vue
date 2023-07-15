a42131ba0449da4bae9f0bf8b9c8b0b17c359f05
guirossi <guizo.rossi@gmail.com>
2023-07-15 15:37:06 -0300

    added instant button component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="app">
     <div class="background-buttons">
-      <!-- <button @click="this.takePicture">TIRE FOTO</button> -->
 
-      <div class="instant" v-for="instant in listOfInstants" :key="instant">
-        <div class="small-button-background" :style="getRandomColor()"></div>
-        <button class="small-button"></button>
-        <div class="small-button-shadow"></div>
-      </div>
+      <!-- <button @click="this.takePicture">TIRE FOTO</button> -->
+      <InstantButton v-for="instant in listOfInstants" :key="instant" />
 
     </div>
   </div>
@@ -16,6 +12,7 @@
 <script>
 /* eslint-disable */
 import { Camera, CameraResultType } from '@capacitor/camera'
+import InstantButton from '../src/components/instant-button/instant-button.vue'
 import { App } from '@capacitor/app'
 
 export default {
@@ -27,13 +24,20 @@ export default {
     return {
       myImage: null,
       listOfInstants: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-      colors: ['#FF0000', '#00FF00', '#0000FF']
+
     }
+  },
+  components: {
+    InstantButton
   },
   mounted: function () {
 
   },
   methods: {
+    getRandomColor: function () {
+      const randomIndex = Math.floor(Math.random() * this.colors.length);
+      return 'background-color: ' + this.colors[randomIndex];
+    },
     takePicture: async function () {
       const image = await Camera.getPhoto({
         quality: 90,
@@ -47,15 +51,21 @@ export default {
         this.myImage = imageUrl;
       }
     },
-    getRandomColor: function () {
-      const randomIndex = Math.floor(Math.random() * this.colors.length);
-      return 'background-color: ' + this.colors[randomIndex];
-    }
   },
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+.app {
+  display: flex;
+  justify-content: center;
+  background-color: #18191A;
+}
+
 .background-buttons {
   width: 300px;
   text-align: center;
@@ -64,57 +74,5 @@ export default {
   justify-content: center;
   text-align: center;
   flex-wrap: wrap;
-}
-
-.app {
-  display: flex;
-  justify-content: center;
-}
-
-.instant {
-  position: relative;
-  vertical-align: top;
-  text-align: center;
-  word-wrap: break-word;
-
-  margin-right: 18px;
-  flex-basis: calc((100% - 60px) / 3);
-  margin-bottom: 100px;
-}
-
-.circle {
-  background-color: #00ff00;
-}
-
-.small-button-background {
-  border-radius: 50%;
-  width: 86px;
-  height: 84px;
-  margin-top: 3px;
-  margin-left: 3px;
-  position: absolute;
-}
-
-.small-button {
-  width: 94px;
-  height: 89px;
-  position: absolute;
-  border: 0;
-  display: block;
-  background: url('/public/up.png') no-repeat;
-  z-index: 1;
-  outline: none;
-}
-
-.small-button:active {
-  background: url('/public/down.png') no-repeat;
-}
-
-.small-button-shadow {
-  width: 94px;
-  height: 89px;
-  margin-bottom: 5px;
-  background: url('/public/shadow.png') no-repeat;
-  position: absolute;
 }
 </style>
---
+++ src/components/instant-button/instant-button.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="instant">
+        <div class="small-button-background" :style="getRandomColor()"></div>
+        <button class="small-button"></button>
+        <div class="small-button-shadow"></div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "instant-button",
+    data: function () {
+        return {
+            colors: ['#FF0000', '#00FF00', '#0000FF']
+        }
+    },
+    methods: {
+        getRandomColor: function () {
+            const randomIndex = Math.floor(Math.random() * this.colors.length);
+            return 'background-color: ' + this.colors[randomIndex];
+        }
+    }
+}
+
+</script>
+
+<style>
+.instant {
+    position: relative;
+    vertical-align: top;
+    text-align: center;
+    word-wrap: break-word;
+
+    margin-right: 18px;
+    flex-basis: calc((100% - 60px) / 3);
+    margin-bottom: 100px;
+}
+
+.small-button-background {
+    border-radius: 50%;
+    width: 86px;
+    height: 84px;
+    margin-top: 3px;
+    margin-left: 3px;
+    position: absolute;
+}
+
+.small-button {
+    width: 94px;
+    height: 89px;
+    position: absolute;
+    border: 0;
+    display: block;
+    background: url('/public/up.png') no-repeat;
+    z-index: 1;
+    outline: none;
+}
+
+.small-button:active {
+    background: url('/public/down.png') no-repeat;
+}
+
+.small-button-shadow {
+    width: 94px;
+    height: 89px;
+    margin-bottom: 5px;
+    background: url('/public/shadow.png') no-repeat;
+    position: absolute;
+    z-index: -1;
+}
+</style>
